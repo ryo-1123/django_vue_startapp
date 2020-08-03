b4b2ef5636ea16bc3b59a71c7e6fed6f17fb5918
ryo-1123 <union.adiinoyr_18@softbank.ne.jp>
2020-08-04 00:13:15 +0900

    fix: file rename

--- app/settings.py
+++ app/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
+    'items.apps.UsersConfig',
+    'store.apps.UsersConfig',
+    'payments.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
